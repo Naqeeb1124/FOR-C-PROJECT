dca830a24d54be8372c503ccbbb788c38e8fecd6
Omar-elmetwalli <164086888+Omar-elmetwalli@users.noreply.github.com>
2024-05-11 15:52:01 +0300

    Update toolbar.h

--- toolbar.h
+++ toolbar.h
@@ -9,6 +9,9 @@ private:
 	int height, width;	
 	string toolbarItemImages[ITM_CNT]; //array to hold paths to images of diffrent items in the toolbar
 	game* pGame;
+	int level = 1;
+	int lives = 5;
+	int score = 0;
 
 public:
 	toolbar(game* r_pGame);
