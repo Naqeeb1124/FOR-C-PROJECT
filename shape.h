7ec3f18babde5826d4748f814081bb4a16e50e7b
Naqeeb1124 <164079169+Naqeeb1124@users.noreply.github.com>
2024-05-11 18:15:59 +0400

    Update shape.h

--- shape.h
+++ shape.h
@@ -4,7 +4,6 @@ using namespace std;
 #include "CMUgraphicsLib\CMUgraphics.h"
 
 class game;     //forward declaration
-
 struct point
 {
 	int x, y;
@@ -30,7 +29,7 @@ enum ShapeType
 	CAR, //BUGATTI RAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH
 	BOAT, //TOP G YACHT RAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH
 	PLANE, //TOP G PRIVATE JET RAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAH
-	Arrow, //TOP G ARROW SHOW US THE WAY TO SUCCESS BIG TOP G RAAAAAAAAAAAAAAAAAAAAAAAAAAAH
+	arrow, //TOP G ARROW SHOW US THE WAY TO SUCCESS BIG TOP G RAAAAAAAAAAAAAAAAAAAAAAAAAAAH
 };
 
 enum direction
@@ -46,30 +45,29 @@ class shape
 {
 protected:
 	point RefPoint;		//Each shape must have a reference point
-    game* pGame;        //pointer to game object
+	game* pGame;        //pointer to game object
 	color fillColor;	//shape fill color
 	color borderColor;	//shape border color
 public:
-    shape(game* r_pGame, point ref);
-    virtual void draw() const=0;//for a shape to draw itself on the screen
+	shape(game* r_pGame, point ref);
+	virtual void draw() const = 0;//for a shape to draw itself on the screen
 	void setRefPoint(point p);
 	virtual void rotate() = 0;
-								  
-							  
+	virtual void move(direction dir);
+	point getRefPoint();
+
+
 	//-- The following functions should be supported by the shape class
 	//-- It should be overridden by each inherited shape
 	//-- Decide the parameters that you should pass to each function	
 
 	/*virtual void rotate() = 0;*/	//Rotate the shape
-	virtual void flip();
-	virtual void resize_up();	//Resize the shape
-	virtual void resize_down();
-	virtual void move(direction dir) ;		//Move the shape
+	virtual void flip()=0;
+	virtual void resize_up()=0;	//Resize the shape
+	virtual void resize_down()=0;
+	/*virtual void move(direction dir)=0;*/		//Move the shape
 	//virtual void save(ofstream &OutFile) = 0;	//Save the shape parameters to the file
 	//virtual void load(ifstream &Infile) = 0;	//Load the shape parameters to the file
+	static point multiplyByMatrix(point& p);
 
 };
-
-
-
-
