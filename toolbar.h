a8404abdc87b36a16989df21db45687cb1ca31e8
Naqeeb1124 <164079169+Naqeeb1124@users.noreply.github.com>
2024-05-22 08:36:00 +0400

    Update toolbar.h

--- toolbar.h
+++ toolbar.h
@@ -6,17 +6,11 @@ class toolbar
 {
 
 private:
-	int height, width;	
+	int height, width;
 	string toolbarItemImages[ITM_CNT]; //array to hold paths to images of diffrent items in the toolbar
 	game* pGame;
-	int level = 1;
-	int lives = 5;
-	int score = 0;
 
 public:
 	toolbar(game* r_pGame);
-	toolbarItem getItemClicked(int x);//handles clicks on toolbar icons, returns the "enum" of the clicekd item
-	int getScore() const;
-
+	toolbarItem getItemClicked(int x);	//handles clicks on toolbar icons, returns the "enum" of the clicekd item
 };
-
